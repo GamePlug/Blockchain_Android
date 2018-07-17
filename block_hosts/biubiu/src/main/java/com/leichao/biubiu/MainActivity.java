9f8d18feb216f0140c71caba7cb865d6b7f95fce
LeiChao <540007827@qq.com>
2018-07-17 14:42:51 +0800

    Update the framework

--- block_hosts/biubiu/src/main/java/com/leichao/biubiu/MainActivity.java
+++ block_hosts/biubiu/src/main/java/com/leichao/biubiu/MainActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.qihoo360.replugin.RePlugin;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RePlugin.startActivity(MainActivity.this, RePlugin.createIntent("biubiu_home", "com.leichao.biubiu.home.MainActivity"));
     }
 }
