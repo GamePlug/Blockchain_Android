19e255a66bf37720e24a6b352d64fd4848b3c05e
LeiChao <540007827@qq.com>
2018-09-26 15:17:43 +0800

    Update processor lib

--- block_libs/processor/src/main/java/com/leichao/processor/MyProcessor.java
+++ block_libs/processor/src/main/java/com/leichao/processor/MyProcessor.java
@@ -5,87 +5,56 @@ import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.TypeSpec;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.Filer;
-import javax.annotation.processing.Messager;
-import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.util.Elements;
 
 @AutoService(Processor.class)
 public class MyProcessor extends AbstractProcessor {
 
-    /**
-     * 文件相关的辅助类
-     */
-    private Filer mFiler;
-    /**
-     * 元素相关的辅助类
-     */
-    private Elements mElementUtils;
-    /**
-     * 日志相关的辅助类
-     */
-    private Messager mMessager;
-
-    /**
-     * 每一个注解处理器类都必须有一个空的构造函数。
-     * 然而，这里有一个特殊的init()方法，它会被注解处理工具调用，
-     * 并输入ProcessingEnviroment参数。
-     *
-     * @param processingEnvironment
-     */
     @Override
-    public synchronized void init(ProcessingEnvironment processingEnvironment) {
-        super.init(processingEnvironment);
-        mElementUtils = processingEnv.getElementUtils();
-        mMessager = processingEnv.getMessager();
-        mFiler = processingEnv.getFiler();
-        String a = "中文会乱码吗？";
-    }
-
-    @Override
-    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
-
-
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         // 类名和包名
-        TypeSpec finderClass = TypeSpec.classBuilder("MyGeneratedClass")
+        TypeSpec finderClass = TypeSpec.classBuilder("MyCustomClass")
                 .addModifiers(Modifier.PUBLIC)
-//                .addSuperinterface(ParameterizedTypeName.get(TypeUtil.INJECTOR, TypeName.get(mClassElement.asType())))
-//                .addMethod(methodBuilder.build())
                 .build();
-
         // 创建Java文件
-        JavaFile javaFile = JavaFile.builder("com.example.alex.annotationprocessordemo", finderClass).build();
-
+        JavaFile javaFile = JavaFile.builder("com.leichao.haha", finderClass)
+                .addFileComment("这是自动生成的类，请不要编辑!")
+                .build();
         try {
-            javaFile.writeTo(mFiler);
+            javaFile.writeTo(processingEnv.getFiler());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
         return true;
     }
 
-    /**这个方法必须重写，否则无法生成Java文件
-     * 这里必须指定，这个注解处理器是注册给哪个注解的。
-     * 注意，它的返回值是一个字符串的集合，包含本处理器想要处理的注解类型的合法全称。
-     * 换句话说，在这里定义你的注解处理器注册到哪些注解上。
-     * @return
-     */
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
     @Override
     public Set<String> getSupportedAnnotationTypes() {
         Set<String> types = new LinkedHashSet<>();
-        types.add(Override.class.getCanonicalName());
-//        types.add(OnClick.class.getCanonicalName());
+        for (Class<? extends Annotation> annotation : getSupportedAnnotations()) {
+            types.add(annotation.getCanonicalName());
+        }
         return types;
+    }
+
+    private Set<Class<? extends Annotation>> getSupportedAnnotations() {
+        Set<Class<? extends Annotation>> annotations = new LinkedHashSet<>();
+        annotations.add(Override.class);// 添加自定义注解
+        return annotations;
     }
 
 }
