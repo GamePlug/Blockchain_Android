53b151fe5981a6f9d4af0d2be4912ef981047e5a
LeiChao <540007827@qq.com>
2018-11-15 19:30:24 +0800

    Update retrofit lib

--- block_libs/retrofit/src/main/java/com/leichao/retrofit/converter/JsonRequestConverter.java
+++ block_libs/retrofit/src/main/java/com/leichao/retrofit/converter/JsonRequestConverter.java
@@ -3,6 +3,7 @@ package com.leichao.retrofit.converter;
 import com.google.gson.Gson;
 import com.leichao.retrofit.util.Constant;
 
+import java.io.File;
 import java.lang.reflect.Type;
 
 import okhttp3.MediaType;
@@ -22,13 +23,34 @@ public final class JsonRequestConverter<T> implements Converter<T, RequestBody> 
 
     @Override
     public RequestBody convert(T value) {
-        String jsonStr = "";
-        try {
-            jsonStr = new Gson().toJson(value, type);
-        } catch (Exception e) {
-            e.printStackTrace();
+        // ConverterFactory中type的class可能在接口中被定义为Object，此处还要判断value真正对应的类型
+        Class clazz = value.getClass();
+        if (clazz == String.class
+                || clazz == boolean.class || clazz == Boolean.class
+                || clazz == byte.class || clazz == Byte.class
+                || clazz == char.class || clazz == Character.class
+                || clazz == double.class || clazz == Double.class
+                || clazz == float.class || clazz == Float.class
+                || clazz == int.class || clazz == Integer.class
+                || clazz == long.class || clazz == Long.class
+                || clazz == short.class || clazz == Short.class) {
+            // 如果是String或者基本类型，则转换成String
+            return new StringRequestConverter().convert((String) value);
+
+        } else if (clazz == File.class) {
+            // 如果是File类型，则转换成File
+            return new FileRequestConverter().convert((File) value);
+
+        } else {
+            // 如果是其他类型，则转换成json
+            String jsonStr = "";
+            try {
+                jsonStr = new Gson().toJson(value, type);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return RequestBody.create(MEDIA_TYPE_JSON, jsonStr);
         }
-        return RequestBody.create(MEDIA_TYPE_JSON, jsonStr);
     }
 
 }
